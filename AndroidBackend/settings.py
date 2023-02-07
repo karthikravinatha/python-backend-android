64018a716e7f7a79a23f19ec4ab16488a91e8ff3
Karthik <karthikravinatha@gmail.com>
2023-02-07 09:30:36 +0530

    Implemented managers features.

--- AndroidBackend/settings.py
+++ AndroidBackend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "sslserver",
+    # "sslserver",
     'rest_framework',
     'corsheaders',
     'sign_up',
